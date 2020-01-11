2a014e6c777216da33f6815add94371fe3160878
Arushan3 <36963236+Arushan3@users.noreply.github.com>
2020-01-11 16:04:59 -0500

    Added seconds and fixed display

--- src/utils/date_utils.js
+++ src/utils/date_utils.js
@@ -1,10 +1,6 @@
-const MERIDIEMS = {
-  AM: 'AM',
-  PM: 'PM',
-};
+function convertToStandardTime(hours, minutes, seconds) {
 
-function convertToStandardTime(hours, minutes) {
-  return (hours > 12) ? `${hours - 12}:${minutes} ${MERIDIEMS.PM}` : `${hours}:${minutes} ${MERIDIEMS.AM}`;
+  return `${addZeros(hours)}:${addZeros(minutes)}:${addZeros(seconds)}`;
 }
 
 function getHoursFromDate(date) {
@@ -14,17 +10,24 @@ function getHoursFromDate(date) {
 function getMinutesFromDate(date) {
   return date.getMinutes();
 }
-
+function getSecondsFromDate(date) {
+  return date.getSeconds();
+}
 function getMonthFromDate(date) {
   return date.getMonth() + 1;
 }
-
+function addZeros(value){
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
 export function getDateFromUTC(utcSeconds) {
   return new Date(utcSeconds * 1000);
 }
 
 export function getDisplayDate(date) {
   const dateShortForm = `${date.getDate()}/${getMonthFromDate(date)}/${date.getFullYear()}`;
-  const timeShortForm = convertToStandardTime(getMinutesFromDate(date), getHoursFromDate(date));
+  const timeShortForm = convertToStandardTime(getHoursFromDate(date),getMinutesFromDate(date),getSecondsFromDate(date));
   return `${dateShortForm} | ${timeShortForm}`;
 }
