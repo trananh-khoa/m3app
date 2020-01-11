a5728e54bfa38a803f5445b80d8109bad9f53510
Khoa Tran <trananh_khoa@outlook.com>
2020-01-11 18:16:03 -0500

    Removed trash from the repo

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,17 @@
   <v-app id='app'>
     <v-content>
       <DataTable/>
-      <FloorPlan/>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import DataTable from '@/components/DataTable.vue';
-import FloorPlan from '@/components/FloorPlan.vue';
 
 export default {
   name: 'App',
   components: {
     DataTable,
-    FloorPlan,
   },
   data: () => ({
   }),
