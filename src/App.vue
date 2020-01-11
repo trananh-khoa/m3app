6d937a9002768b6a39581baa50301b90ccbc67c7
Khoa Tran <trananh_khoa@outlook.com>
2020-01-11 13:18:58 -0500

    Added Vuetify library

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <div id="app">
-    <h1>Hello World</h1>
-  </div>
+  <v-app>
+    <v-content>
+      <h1>Hello World!</h1>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: 'App',
+  components: {
+  },
+  data: () => ({
+  }),
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
