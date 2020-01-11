479927d7793e4b1e975c4a3c72a1ec08523dfcc7
Khoa Tran <trananh_khoa@outlook.com>
2020-01-11 17:04:25 -0500

    Fixed date format for raw data table

--- src/utils/date_utils.js
+++ src/utils/date_utils.js
@@ -1,5 +1,9 @@
+function padWithZeros(value) {
+  return (value < 10) ? `0${value}` : value;
+}
+
 function convertToStandardTime(hours, minutes, seconds) {
-  return `${addZeros(hours)}:${addZeros(minutes)}:${addZeros(seconds)}`;
+  return `${padWithZeros(hours)}:${padWithZeros(minutes)}:${padWithZeros(seconds)}`;
 }
 
 function getHoursFromDate(date) {
@@ -9,24 +13,25 @@ function getHoursFromDate(date) {
 function getMinutesFromDate(date) {
   return date.getMinutes();
 }
+
 function getSecondsFromDate(date) {
   return date.getSeconds();
 }
+
 function getMonthFromDate(date) {
   return date.getMonth() + 1;
 }
-function addZeros(value){
-  if (value < 10) {
-    value = "0" + value;
-  }
-  return value;
-}
+
 export function getDateFromUTC(utcSeconds) {
   return new Date(utcSeconds * 1000);
 }
 
 export function getDisplayDate(date) {
-  const dateShortForm = `${date.getDate()}/${getMonthFromDate(date)}/${date.getFullYear()}`;
-  const timeShortForm = convertToStandardTime(getHoursFromDate(date),getMinutesFromDate(date),getSecondsFromDate(date));
+  const dateShortForm = `${padWithZeros(date.getDate())}/${padWithZeros(getMonthFromDate(date))}/${padWithZeros(date.getFullYear())}`;
+  const timeShortForm = convertToStandardTime(
+    getHoursFromDate(date),
+    getMinutesFromDate(date),
+    getSecondsFromDate(date),
+  );
   return `${dateShortForm} | ${timeShortForm}`;
 }
