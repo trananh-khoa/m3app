bf03f219679ed54a23a63854c7c2e7943aa24134
Khoa Tran <trananh_khoa@outlook.com>
2020-01-11 14:20:29 -0500

    Added DataTable headers

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,10 +1,26 @@
 <template>
-  <h1>This is the DataTable component!</h1>
+  <v-data-table
+    :headers='headers'
+    :items='items'
+  >
+  </v-data-table>
 </template>
 
 <script>
+const HEADERS = [
+  { text: 'Date (dd/mm/yyyy h:m)', value: 'date' },
+  { text: 'Device', value: 'device' },
+  { text: 'Device ID', value: 'deviceID' },
+  { text: 'Event', value: 'event' },
+  { text: 'Guest ID', value: 'guestID' },
+];
+
 export default {
   name: 'datatable',
+  data: () => ({
+    headers: HEADERS,
+    items: [],
+  }),
 };
 </script>
 
