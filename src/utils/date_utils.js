7cdabf6124d394de8454aad2b07ed9774f539a11
Khoa Tran <trananh_khoa@outlook.com>
2020-01-11 14:58:22 -0500

    Fixed date utils and cleaned up formatting

--- src/utils/date_utils.js
+++ src/utils/date_utils.js
@@ -3,16 +3,19 @@ const MERIDIEMS = {
   PM: 'PM',
 };
 
-function convertToStandardTime(hours, minutes){
+function convertToStandardTime(hours, minutes) {
   return (hours > 12) ? `${hours - 12}:${minutes} ${MERIDIEMS.PM}` : `${hours}:${minutes} ${MERIDIEMS.AM}`;
 }
+
 function getHoursFromDate(date) {
   return date.getHours() + 1;
 }
-function getMinutesFromDate(date){
+
+function getMinutesFromDate(date) {
   return date.getMinutes();
 }
-function getMonthFromDate(date){
+
+function getMonthFromDate(date) {
   return date.getMonth() + 1;
 }
 
@@ -21,8 +24,7 @@ export function getDateFromUTC(utcSeconds) {
 }
 
 export function getDisplayDate(date) {
-  const minutes = getMinutesFromDate(date);
-  const hours = getHoursFromDate(date);
   const dateShortForm = `${date.getDate()}/${getMonthFromDate(date)}/${date.getFullYear()}`;
-  const timeShortForm = convertToStandardTime(hours,minutes);
+  const timeShortForm = convertToStandardTime(getMinutesFromDate(date), getHoursFromDate(date));
+  return `${dateShortForm} | ${timeShortForm}`;
 }
