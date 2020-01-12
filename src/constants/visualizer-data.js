e86a2162062e9945d4ebabc37331413a533c1efb
Arushan3 <36963236+Arushan3@users.noreply.github.com>
2020-01-11 20:08:34 -0500

    Added suspendlist

--- src/constants/visualizer-data.js
+++ src/constants/visualizer-data.js
@@ -1,2 +1,17 @@
 // Get a list of all the suspects (including N/A)
 // Loop through the JavaScript Object and get all unique Guest IDs and export an array
+import SuspectDataList from '@/constants/data.js' 
+
+export function getGuestList(){
+    const suspectList = [];
+    const listKeys = Object.keys(SuspectDataList);
+   
+    listKeys.forEach((key) => {
+        const guest = SuspectDataList[key]['guest-id'];
+        if(!(suspectList.includes(guest))){
+            suspectList.push({
+                guestID: guest,
+            });
+        }
+      });
+}
